ba497ae17f7372f4518f83363d38d21824be630d
Brad Hambrice <hambrice@uab.edu>
2018-01-31 22:07:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,24 @@
+require 'pry'
 class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+
+    if params["figure"]["title_ids"] != nil
+      params["figure"]["title_ids"].each do |title|
+        @figure.titles << Title.find(title)
+      end
+    end
+      if params["figure"]["landmark_ids"] != nil
+      params["figure"]["landmark_ids"].each do |landmark|
+        @figure.landmarks << Landmark.find(landmark)
+      end
+    end
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"]) if !params["landmark"]["name"].empty?
+  @figure.titles << Title.create(name: params["title"]["name"]) if !params["title"]["name"].empty?
+  @figure.save
   end
 end
