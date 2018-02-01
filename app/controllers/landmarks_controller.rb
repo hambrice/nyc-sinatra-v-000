ffa239862bbe8fe124c830f75205fae530e32317
Brad Hambrice <hambrice@uab.edu>
2018-02-01 00:13:13 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,4 +24,10 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
     erb :'landmarks/edit'
   end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    redirect "/landmarks/#{@landmark.id}"
+  end
 end
